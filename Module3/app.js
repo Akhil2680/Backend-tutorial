fa6ed2520dee61ad8ebc7d26331be3605c485f3a
Akhil2680 <akhilvikramsingh18@gmail.com>
2026-01-07 23:48:03 +0530

    hiii

--- Module3/app.js
+++ Module3/app.js
@@ -1,48 +1,14 @@
-const express=require("express");
-const app=express();
-const Path=require("path");
-const productModel=require("./models/product"); 
-// Middleware
-app.set("view engine","ejs");
-app.use(express.json());
-app.use(require("express").urlencoded({extended:true}));
-app.use(express.static(Path.join(__dirname,"public")));
-app.use(express.static("public"));  
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// Route: Show home page
-app.get("/",(req,res)=>{
-   res.render("index"); 
+app.get('/', (req, res) => {
+  res.cookie('name', 'akhil');
+  res.send('Hello World!');
 });
 
-app.get('/products', async (req, res) => {
-   let allProducts = await productModel.find({});
-   res.render("products", { products: allProducts });
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
 
-app.post('/add-product', async (req, res) => {
-  let {name, price, description}=req.body;
-  let createdProduct = await productModel.create({
-     name,
-     price,
-     description
-});
-   res.send(createdProduct);
-});
-
-app.post('/delete-product/:id', async (req, res) => {
-   let productId = req.params.id;
-   await productModel.findByIdAndDelete(productId);
-   res.redirect('/products');
-});
-
-app.post('/update-product/:id', async (req, res) => {
-   let productId = req.params.id;
-   let {name, price, description} = req.body;   
-
-// Start the server
-
-
-
-app.listen(3000,()=>{
-   console.log("Server is running on http://localhost:3000");
-});
+module.exports = app;
