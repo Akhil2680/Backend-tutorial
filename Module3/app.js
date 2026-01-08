38f144930b31d77e4f04a7c9ab003b47ac1f9fae
Akhil2680 <akhilvikramsingh18@gmail.com>
2026-01-08 22:07:05 +0530

    updated readme

--- Module3/app.js
+++ Module3/app.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
 
-app.get('/', (req, res) => {
-  res.cookie('name', 'akhil');
-  res.send('Hello World!');
-});
+// app.get('/', (req, res) => {
+//   res.cookie('name', 'akhil');
+//   res.send('Hello World!');
+// });
+
+  //  app.get('/', function(req, res) {
+  //   bcrypt.genSalt(10, function(err, salt) {
+        
+  //       bcrypt.hash('myPlaintextPassword', salt, function(err, hash){
+  //         console.log('Hash:', hash);
+  //       });
+  //   });
+  // });
+
+  app.get('/', function(req, res) {
+    
+        
+        bcrypt.compare('myPlaintextPassword', " $2b$10$Kob4PseDA3qOUr5i1nnWyefKoc1c8p.Y2luy/j2yG3JJsLmNShCn", function(err, result){
+          console.log('result:', result);
+        
+    });
+  });
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-module.exports = app;
