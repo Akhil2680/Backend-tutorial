02a3532908564a5551cd228cd0cc9377a1138394
Akhil2680 <akhilvikramsingh18@gmail.com>
2026-01-10 23:38:25 +0530

    JWT tocken

--- Module3/app.js
+++ Module3/app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
+const jwt = require('jwt');
 
 // app.get('/', (req, res) => {
 //   res.cookie('name', 'akhil');
@@ -18,16 +19,22 @@ const cookieParser = require('cookie-parser');
   //   });
   // });
 
-  app.get('/', function(req, res) {
+  // app.get('/', function(req, res) {
     
         
-        bcrypt.compare('myPlaintextPassword', " $2b$10$Kob4PseDA3qOUr5i1nnWyefKoc1c8p.Y2luy/j2yG3JJsLmNShCn", function(err, result){
-          console.log('result:', result);
+  //       bcrypt.compare('myPlaintextPassword', " $2b$10$Kob4PseDA3qOUr5i1nnWyefKoc1c8p.Y2luy/j2yG3JJsLmNShCn", function(err, result){
+  //         console.log('result:', result);
         
-    });
-  });
+  //   });
+  // });
 
-
+app.get('/', (req, res) => {
+  const token = jwt.sign({ foo: 'bar' }, 'shhhhh');
+  console.log('Token:', token);
+  const decoded = jwt.verify(token, 'shhhhh');
+  console.log('Decoded:', decoded);
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
